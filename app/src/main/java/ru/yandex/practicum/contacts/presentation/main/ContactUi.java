e3f2080d6b7f109b847abd85cfeba5c495136833
Lion <lliioonnss@yandex.ru>
2023-03-18 00:06:47 +0300

    Внесены изменения в код согласно второй проектной работе 4-ого спринта

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactUi.java
@@ -5,8 +5,9 @@ import androidx.annotation.NonNull;
 import java.util.List;
 
 import ru.yandex.practicum.contacts.model.ContactType;
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
 
-public class ContactUi {
+public class ContactUi implements ListDiffInterface<ContactUi> {
 
     private final String name;
     private final String phone;
@@ -23,6 +24,10 @@ public class ContactUi {
         this.phone = phone;
         this.photo = photo;
         this.types = types;
+    }
+
+    public boolean theSameAs(ContactUi nub2) {
+        return this.hashCode() == nub2.hashCode();
     }
 
     public String getName() {
